28039a26383dfc13f21b2754a3655cca33018195
Eli <elizabethtrinitygs@gmail.com>
2022-01-14 14:01:08 -0600

    save

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 //console.log('HOLIII CONSOLI !! :D');
+
+//1.- hacer un array con las imagenes del juego
+const cardsGroups = [
+    {
+        name: 'Black Widow',
+        img: 'img/black-widow.jpg'
+    },
+    {
+        name: 'Gamora',
+        img: 'img/gamora.jpg'
+    },
+    {
+        name: 'Wonder Woman',
+        img: 'img/mujer-maravilla.jpg'
+    },
+    {
+        name: 'Raven',
+        img: 'img/raven.jpg'
+    },
+    {
+        name: 'Scarlet Witch',
+        img: 'img/scarlet-witch.jpg'
+    },
+    {
+        name: 'Start Fire',
+        img: 'img/startfire.jpg'
+    },
+    {
+        name: 'Storm',
+        img: 'img/tormenta.jpg'
+    },
+    {
+        name: 'Zatanna',
+        img: 'img/zatanna.jpg'
+    }
+];
+
+
